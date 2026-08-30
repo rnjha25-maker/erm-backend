b715d35efd04e2dc49c2de2ffcede6e58eadd439
LAPTOP-TMHBLSDQ\ajeet kumar yadav <ajeetyadav@outlook.com>
2026-08-30 21:05:34 +0530

    Fix storage dependency packaging

--- ERM_backend/src/main/java/ermorg/erm/model/RiskReview.java
+++ ERM_backend/src/main/java/ermorg/erm/model/RiskReview.java
@@ -16,10 +16,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Entity
 @Table(name = "risk_reviews")
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class RiskReview extends BaseModel {
 
 	@ManyToOne
